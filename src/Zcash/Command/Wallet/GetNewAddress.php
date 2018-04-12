a3d7673d8372b02c07bfe83fcd2aa5923fe0c60b
Nathan King <vherus@users.noreply.github.com>
2018-04-12 17:23:18 +0100

    Remove redundant params getnewaddress

--- src/Zcash/Command/Wallet/GetNewAddress.php
+++ src/Zcash/Command/Wallet/GetNewAddress.php
@@ -8,20 +8,12 @@ final class GetNewAddress implements Command
 {
     private const METHOD = 'getnewaddress';
 
-    /** @var mixed[]|null */
-    private $params;
-
-    public function __construct(?array $params)
-    {
-        $this->params = $params;
-    }
-
     public function jsonSerialize(): object
     {
-        return (object) array_merge([
+        return (object) [
             'jsonrpc' => Command::JSON_RPC_VERSION,
             'id'      => Command::ID,
             'method'  => self::METHOD,
-        ], !is_null($this->params) ? ['params' => $this->params] : []);
+        ];
     }
 }
--- tests/Zcash/Command/Wallet/GetNewAddressTest.php
+++ tests/Zcash/Command/Wallet/GetNewAddressTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class GetNewAddressTest extends TestCase
 {
-    public function test_serialize_without_params_constructs_expected_object()
+    public function test_serialize_constructs_expected_object()
     {
         $expected = (object) [
             'jsonrpc' => '1.0',
@@ -14,18 +14,6 @@ class GetNewAddressTest extends TestCase
             'method'  => 'getnewaddress',
         ];
 
-        $this->assertEquals($expected, (new GetNewAddress(null))->jsonSerialize());
-    }
-
-    public function test_serialize_with_params_constructs_expected_object()
-    {
-        $expected = (object) [
-            'jsonrpc' => '1.0',
-            'id'      => 'curl',
-            'method'  => 'getnewaddress',
-            'params' => ['myparam1', 19, true]
-        ];
-
-        $this->assertEquals($expected, (new GetNewAddress(['myparam1', 19, true]))->jsonSerialize());
+        $this->assertEquals($expected, (new GetNewAddress)->jsonSerialize());
     }
 }
